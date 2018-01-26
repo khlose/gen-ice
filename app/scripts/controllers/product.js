004804bb2b1d72a155fc27fb1d2a78e6bca3ffd5
khlose <schwin60@hotmail.com>
2018-01-26 11:54:17 -0500

    deetail page need decoration

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -8,10 +8,12 @@
  * Controller of the genIceApp
  */
 angular.module('genIceApp')
-  .controller('ProductCtrl', function ($routeParams,$firebaseObject) {
-  console.log($routeParams.sku);
+  .controller('ProductCtrl', function ($scope,$routeParams,$firebaseObject) {
   const ref = firebase.database().ref().child('products').child('icemaker');
+  var prod = $firebaseObject(ref.orderByChild('model').equalTo($routeParams.sku).limitToFirst(1));
   
-  this.product = $firebaseObject(ref.orderByChild('model').equalTo($routeParams.sku));
+  prod.$loaded().then(function(){
+    $scope.currentProduct = prod[$routeParams.sku];
+  });
   
   });
