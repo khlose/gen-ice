656dfa86f4e63e1f4545497c313170cbe223f6a1
khlose <schwin60@hotmail.com>
2018-01-27 10:34:34 -0500

    almost over

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -8,12 +8,25 @@
  * Controller of the genIceApp
  */
 angular.module('genIceApp')
-  .controller('ProductCtrl', function ($scope,$routeParams,$firebaseObject) {
+  .controller('ProductCtrl', function ($scope,$routeParams,$firebaseObject,$timeout) {
   const ref = firebase.database().ref().child('products').child('icemaker');
   var prod = $firebaseObject(ref.orderByChild('model').equalTo($routeParams.sku).limitToFirst(1));
   
   prod.$loaded().then(function(){
     $scope.currentProduct = prod[$routeParams.sku];
   });
+  
+  
+  $scope.added = false;
+  $scope.add = function(product){
+    //if(cart.$add(product, 1)){
+    if(true){  
+      $scope.added = true;
+      $timeout(function(){
+        $scope.added = false;
+      },2000); // Amount of time to show added to cart success message
+    }
+    
+  };
   
   });
