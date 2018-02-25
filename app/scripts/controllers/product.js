a8f5a599a74893b2f4ce9d03fea40f0f050b72e6
khlose <schwin60@hotmail.com>
2018-02-25 11:33:41 -0500

    stuck at email sending shit

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -63,6 +63,38 @@ angular.module('genIceApp')
         if(result.value)
           window.location.href = "#!/contact";
       });;
+      
+/*      swal({
+        title: 'Submit email to run ajax request',
+        input: 'email',
+        showCancelButton: true,
+        confirmButtonText: 'Submit',
+        showLoaderOnConfirm: true,
+        preConfirm: (email) => {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              if (email === 'taken@example.com') {
+                swal.showValidationError(
+                  'This email is already taken.'
+                )
+              }
+              resolve()
+            }, 2000)
+          })
+        },
+        allowOutsideClick: () => !swal.isLoading()
+      }).then((result) => {
+        if (result.value) {
+          swal({
+            type: 'success',
+            title: 'Ajax request finished!',
+            html: 'Submitted email: ' + result.value
+          })
+        }
+      })*/
+      
+      
+      
     }
   };
   
