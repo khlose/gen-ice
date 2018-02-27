b066b7f3bcefa9d9ee605d8b2dd169d9dbfb9885
khlose <schwin60@hotmail.com>
2018-02-27 04:25:35 -0500

    calculator? done

--- app/scripts/controllers/calculator.js
+++ app/scripts/controllers/calculator.js
@@ -10,7 +10,15 @@
 angular.module('genIceApp')
   .controller('CalculatorCtrl', function ($firebaseObject,$scope) {
   
-
+    $scope.ice_amount = 200;
+    $scope.ice_price = 2.5;
+  $scope.elec_cost = 4.20;
+  $scope.water_cost = 15.80;
+  $scope.discount = 0;
+  
+      $scope.per_day_ice_cost = $scope.ice_amount * $scope.ice_price;
+  $scope.per_month_ice_cost = $scope.per_day_ice_cost *30;
+  
   
     const rootRef = firebase.database().ref().child('products');
     const ref = rootRef.child('icemaker');
@@ -18,39 +26,83 @@ angular.module('genIceApp')
     var rawProd = $firebaseObject(ref);
     
  
-    //console.log($scope.selected);
-    //console.log(rawProd);
     $scope.products = [];
     rawProd.$loaded().then(function(){
       angular.forEach(rawProd,function(key,value){
         $scope.products.push({key:value,value:key});
       
       });
+      $scope.selected = $scope.products[0];
+      
+      
+      $scope.daily_elec_unit = $scope.selected.value.daily_elec_unit;
+      $scope.daily_water_unit = $scope.selected.value.daily_water_unit;
+      
+      $scope.daily_elec_cost = ($scope.daily_elec_unit * $scope.elec_cost).toFixed(2);
+      $scope.daily_water_cost = ($scope.daily_water_unit * $scope.water_cost).toFixed(2);
+      
+      $scope.production_rate = $scope.selected.value.production;
+      $scope.icetype = $scope.selected.value.icetype;
+      $scope.capacity = $scope.selected.value.capacity;
+      $scope.dimension = $scope.selected.value.dimension;
+      
+      $scope.daily_utility = (parseFloat($scope.daily_elec_cost) + parseFloat($scope.daily_water_cost)).toFixed(2);
+      $scope.monthly_utility = ($scope.daily_utility * 30).toFixed(2);
+      
+      $scope.monthly_save = (parseFloat($scope.per_month_ice_cost) - parseFloat($scope.monthly_utility)).toFixed(2);
+      
+      $scope.break_even = (parseFloat($scope.selected.value.price * (100-$scope.discount)/100)/parseFloat($scope.monthly_save)).toFixed(2);   
     });
-    console.log($scope.products);
-    $scope.selected = $scope.products[0];
-  
-    $scope.ice_amount = 0;
-    $scope.ice_price = 0;
-  $scope.elec_cost = 4;
-  $scope.water_cost = 15.8;
-  
-  console.log($scope.selected);
 
+
+    
   
   
-    $scope.per_month_ice_cost = $scope.ice_amount * $scope.ice_price * 30;
-    $scope.calculate = function(){
+  $scope.calculate = function(){
       console.log($scope.ice_amount);
     }
     
+  
+  
+  
     $scope.update_monthly_ice = function(){
       /*$scope.per_month_ice_cost = $scope.ice_amount * $scope.ice_price * 30;*/
-      console.log("update1");
-      $scope.per_month_ice_cost = $scope.ice_amount * $scope.ice_price;
+      $scope.per_day_ice_cost = $scope.ice_amount * $scope.ice_price;
+      $scope.per_month_ice_cost = $scope.per_day_ice_cost*30;
+      
+            $scope.monthly_save = (parseFloat($scope.per_month_ice_cost) - parseFloat($scope.monthly_utility)).toFixed(2);
+      
+            $scope.break_even = (parseFloat($scope.selected.value.price * (100-$scope.discount)/100)/parseFloat($scope.monthly_save)).toFixed(2);
+      console.log($scope.discount);
+      
+      
     }
+    
+    
+    
+    
     $scope.update_electricity_water_cost = function(){
-      console.log("update2");
+      
+      $scope.daily_elec_unit = $scope.selected.value.daily_elec_unit;
+      $scope.daily_water_unit = $scope.selected.value.daily_water_unit;
+      
+      $scope.daily_elec_cost = ($scope.daily_elec_unit * $scope.elec_cost).toFixed(2);
+      $scope.daily_water_cost = ($scope.daily_water_unit * $scope.water_cost).toFixed(2);
+            $scope.production_rate = $scope.selected.value.production;
+      $scope.icetype = $scope.selected.value.icetype;
+      $scope.capacity = $scope.selected.value.capacity;
+      $scope.dimension = $scope.selected.value.dimension;
+      
+      $scope.daily_utility = (parseFloat($scope.daily_elec_cost) + parseFloat($scope.daily_water_cost)).toFixed(2);
+      $scope.monthly_utility = ($scope.daily_utility * 30).toFixed(2);
+      
+      
+      $scope.monthly_save = (parseFloat($scope.per_month_ice_cost) - parseFloat($scope.monthly_utility)).toFixed(2);
+      
+      $scope.break_even = (parseFloat($scope.selected.value.price * (100-$scope.discount)/100)/parseFloat($scope.monthly_save)).toFixed(2);
+      
+      
+      console.log($scope.discount);
       
     }
   
