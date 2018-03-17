b680a3afa2b04d52b67dfb1eb82cb2502b042d5e
schwin <schwin60@hotmail.com>
2018-03-17 12:45:39 +0700

    banner + world map left

--- app/scripts/controllers/why.js
+++ app/scripts/controllers/why.js
@@ -8,10 +8,43 @@
  * Controller of the genIceApp
  */
 angular.module('genIceApp')
-  .controller('WhyCtrl', function () {
+  .controller('WhyCtrl', function ($scope,$uibModal) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+  
+  
+  
+  $scope.featuredThumbsClicked = function(buttonNo){
+    var modalInstance = null;
+    var templateUrlLocation = '';
+    switch(buttonNo){
+      case 1:
+        templateUrlLocation = 'views/partsModal.html';
+        break;
+      case 2:
+        templateUrlLocation = 'views/intelligenceModal.html';
+        break;
+      default:
+        break;
+    }
+    modalInstance = $uibModal.open({
+      animation: $scope.animationEnabled,
+      templateUrl:templateUrlLocation,
+      controller:'ModalInstanceCtrl',
+      resolve:{
+        models: function(){
+        }
+      }
+    });
+  }
+  
+  
+  
+  })  .controller('ModalInstanceCtrl', function ($scope, models,$uibModalInstance){
+    $scope.ok = function () {
+    $uibModalInstance.close();
+    }
   });
